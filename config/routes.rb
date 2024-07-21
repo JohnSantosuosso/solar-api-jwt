3567d78522962aecdbc97beec140932477a342fe
John Santosuosso <john.santosuosso1@gmail.com>
2024-07-21 17:55:43 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/users", to: "users#create"
+  post "me", to: "users#me"
+  post "/auth/login", to: "auth#login"
 end
